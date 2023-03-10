bfb2cf03f56a819b407ac8df3f15ea1c9c381e14
unknown <mohamed101291@gmail.com>
2023-03-10 19:34:29 +0200

    js

---
+++ Tasks/js/divisionArrayOfNumber.js
@@ -0,0 +1,16 @@
+let arr = [100,698,546,433,565,6546,644,454,454,655,551,54 ];
+
+function sumArray(array){
+    let sum = 0;
+    for (let i = 0; i < array.length; i++){
+      sum += array[i];
+    }
+    console.log(sum);
+    return(sum);
+}
+
+let sum = sumArray(arr);
+
+let resultOfDivision  = sum / arr.length ;
+
+console.log(resultOfDivision);
